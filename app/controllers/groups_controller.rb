83683cdfdb0494da7c11eef2754a723ae1e93a07
weichang <a376836@gmail.com>
2015-03-10 15:00:22 +0800

    Install devise

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,13 @@
 class GroupsController < ApplicationController
+	before_action :find_group , :only => [:show , :edit , :update , :destroy ]
+	before_action :authenticate_user! ,:only => [:show , :edit , :update , :destroy ]
 
 	def index
 		@groups = Group.all
 	end
 
 	def show
-		@group = Group.find(params[:id])
+		#@group = Group.find(params[:id])
 		@posts = @group.posts
 	end
 
@@ -14,7 +16,7 @@ class GroupsController < ApplicationController
 	end
 
 	def create
-		@group = Group.new(group_params)
+		#@group = Group.new(group_params)
 
 		if @group.save
 			redirect_to groups_path, :notice => '新增討論版成功'
@@ -24,12 +26,12 @@ class GroupsController < ApplicationController
 	end
 
 	def edit
-		@group = Group.find(params[:id])
+		#@group = Group.find(params[:id])
 	end
 
 	def update
 
-		@group = Group.find(params[:id])
+		#@group = Group.find(params[:id])
 
 		if @group.update(group_params)
 			
@@ -40,14 +42,16 @@ class GroupsController < ApplicationController
 	end
 
 	def destroy
-		@group = Group.find(params[:id])
+		#@group = Group.find(params[:id])
 		@group.destroy
 
     	redirect_to groups_path, :alert => '討論版已刪除'
 	end
 
 	private
-
+	def find_group
+		@group = Group.find(params[:id])
+	end
 	def group_params
 		params.require(:group).permit(:title,:description)
 	end
