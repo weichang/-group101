0475d3a78e4a27e4917e3325268b51db7d29d9e9
weichang <a376836@gmail.com>
2015-03-10 12:18:33 +0800

    change posts_controller add before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,14 @@
 class PostsController < ApplicationController
-
-	def show
-		
-	end
+	before_action :find_group
 
 	def new
-		@group = Group.find(params[:group_id])
+	
 		@post = @group.posts.new
 	end
 
 	def create
 
-		@group = Group.find(params[:group_id])
+		
 		@post = @group.posts.new(post_params)
 
 		if @post.save
@@ -24,12 +21,12 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@group = Group.find(params[:group_id])
+		
 		@post = @group.posts.find(params[:id])
 	end
 
 	def update
-		@group = Group.find(params[:group_id])
+		
 		@post = @group.posts.find(params[:id])
 
 		if @post.update(post_params)
@@ -42,7 +39,7 @@ class PostsController < ApplicationController
 	end
 
 	def destroy
-		@group = Group.find(params[:group_id])
+		
 		@post = @group.posts.find(params[:id])
 		@post.destroy
     	redirect_to group_path(@group), :alert => '文章已刪除'
@@ -50,6 +47,10 @@ class PostsController < ApplicationController
 
 	private 
 
+	def find_group
+
+		@group = Group.find(params[:group_id])		
+	end
 	def post_params
 
 		params.require(:post).permit(:content)	
